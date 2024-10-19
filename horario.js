6998da9160fde79b30fc81ef0059d40ffddfe313
unknown <gupiochenry16@gmail.com>
2024-10-19 09:50:08 -0500

    se agrega mas html

---
+++ horario.js
@@ -0,0 +1,6 @@
+
+document.getElementById("years").addEventListener("change", function() {
+    
+    alert("Has seleccionado el año: " + selectedYear);
+    // Aquí puedes agregar lógica para cambiar el contenido según el año seleccionado
+});
